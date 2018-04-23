1a892b8e325534cb5fd4dd116acbd9e54e9b50fa
Alejandro García <agarciavallecillo@gmail.com>
2018-04-23 16:13:46 +0200

    Initial commit

---
+++ Seegood/ActivitiesViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Seegood
+//
+//  Created by Alejandro Garcia Vallecillo on 23/4/18.
+//  Copyright © 2018 Alejandro Garcia Vallecillo. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
